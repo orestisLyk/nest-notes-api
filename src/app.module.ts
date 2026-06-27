e14dab7ada62edbe4674acbb4843d5a4e0278004
orestisLyk <orestis.lyk@outlook.com>
2026-06-27 13:53:06 +0300

    chore: configure app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotesModule } from './notes/notes.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { NotesController } from './notes/notes.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [NotesModule],
-  controllers: [NotesModule],
+  imports: [PrismaModule, NotesModule, UsersModule],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
