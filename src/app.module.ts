68abbc2b9b63be60510548f74d58ccc692da5c70
orestisLyk <orestis.lyk@outlook.com>
2026-06-27 20:50:59 +0300

    chore: add authmodule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { NotesController } from './notes/notes.controller';
 import { UsersController } from './users/users.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, NotesModule, UsersModule],
+  imports: [PrismaModule, NotesModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
