5abe2875171e0171139cb3a0b25793aae9baa77b
orestisLyk <orestis.lyk@outlook.com>
2026-06-27 13:55:16 +0300

    feat: add note dtos

---
+++ src/notes/dto/create-note.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, MinLength } from "class-validator";
+
+export class CreateNoteDto {
+    @IsString()
+    @MinLength(1)
+    title!: string;
+    @IsString()
+    @MinLength(1)
+    content!: string;
+    @IsString()
+    userId!: string;
+}
