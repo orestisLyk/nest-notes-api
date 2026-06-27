dc5b3d5d5c357e00465fb40b13705cd3ea3fa803
orestisLyk <orestis.lyk@outlook.com>
2026-06-27 13:53:33 +0300

    feat: add userservice logic

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { NoteReadonlyDto } from './dto/note-readonly.dto';
+import { CreateNoteDto } from './dto/create-note.dto';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+    constructor(private prisma: PrismaService) {}
+
+    async getAllByUserId(userId: string) : Promise<NoteReadonlyDto[]> {
+        const notes = await this.prisma.note.findMany({
+            where: { userId }
+        });
+        return notes.map(note => ({
+            id: note.id,
+            title: note.title,
+            content: note.content,
+            createdAt: note.createdAt,
+            updatedAt: note.updatedAt,
+            userId: note.userId
+        }));
+    }
+
+    async getById(id: string) : Promise<NoteReadonlyDto> {
+        const note = await this.prisma.note.findUnique({
+            where: { id }
+        });
+        if (!note) {
+            throw new NotFoundException(`Note with ID ${id} not found`);
+        }
+        return {
+            id: note.id,
+            title: note.title,
+            content: note.content,
+            createdAt: note.createdAt,
+            updatedAt: note.updatedAt,
+            userId: note.userId
+        };
+    }
+
+    async createNote(dto: CreateNoteDto) : Promise<NoteReadonlyDto> {
+        const newNote = await this.prisma.note.create({
+            data: {
+                title: dto.title,
+                content: dto.content,
+                userId: dto.userId,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        });
+        return {
+            id: newNote.id,
+            title: newNote.title,
+            content: newNote.content,
+            createdAt: newNote.createdAt,
+            updatedAt: newNote.updatedAt,
+            userId: newNote.userId
+        };
+
+    }
+}
