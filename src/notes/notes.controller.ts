ebfff077a3bf731ccde3bd5608800c3991b4fd2a
orestisLyk <orestis.lyk@outlook.com>
2026-06-27 19:27:01 +0300

    feat: add notes controller logic

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { NotesService } from './notes.service';
+import { CreateNoteDto } from './dto/create-note.dto';
 
 @Controller('notes')
-export class NotesController {}
+export class NotesController {
+    constructor(private notesService: NotesService) {}
+
+    @Get("/by-user/:userId")
+    async getNotesByUserId(@Param('userId') userId: string) {
+        return this.notesService.getAllByUserId(userId);
+    }
+
+    @Get(':id')
+    async getNoteById(@Param('id') id: string) {
+        return this.notesService.getById(id);
+    }
+
+    @Post()
+    async createNote(@Body() dto: CreateNoteDto) {
+        return this.notesService.createNote(dto);
+    }
+
+}
